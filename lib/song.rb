d15cda0d0134db656690d191fef7a59fe2c7caaf
Drew Thompson <drewallynthompson@gmail.com>
2020-09-05 23:54:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,5 +16,10 @@ class Song
     song
   end
     
-  def self.new_by_name  
+  def self.new_by_name(song_name)
+    song_name = self.name
+    song.name = song_name
+    song.name
+  end
+    
 end
