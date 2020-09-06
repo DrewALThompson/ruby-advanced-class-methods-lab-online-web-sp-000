a2b5826d2e9d105860f4b8199bac4317aad5bc95
Drew Thompson <drewallynthompson@gmail.com>
2020-09-06 00:12:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,10 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name)? song : self.create_by_name
+    if self.find_by_name(song_name)? 
+      song 
+    else 
+      self.create_by_name(song_name)
     end
   end
   
